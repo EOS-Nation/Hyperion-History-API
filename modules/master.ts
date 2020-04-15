24c09c6b60593138c0c26d8c6dc33ba51585a47f
Frederik Schöll <fschoell@posteo.de>
2020-04-15 14:02:42 +0200

    move index templates to /etc directory

--- modules/master.ts
+++ modules/master.ts
@@ -1246,7 +1246,7 @@ export class HyperionMaster {
             }
         ];
 
-        const indexConfig = await import('../definitions/index-templates');
+        const indexConfig = await import('/etc/hyperion/definitions/index-templates');
 
         const indicesList = [
             {name: "action", type: "action"},
